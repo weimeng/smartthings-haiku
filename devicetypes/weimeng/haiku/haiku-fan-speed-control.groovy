409b816627295e77b90e026c2c9618a33322867c
Wei-Meng Lee <weimeng@staticfusion.org>
2018-02-20 23:20:36 +0800

    Update tiles and control scheme
    - Tiles are updated to recognise that ST doesn't know fan state
    - Switched capability from Fan Speed to Switch Level for voice control

--- devicetypes/weimeng/haiku/haiku-fan-speed-control.groovy
+++ devicetypes/weimeng/haiku/haiku-fan-speed-control.groovy
@@ -12,12 +12,12 @@ preferences {
 
 metadata {
 	definition (name: "Haiku Fan Speed Control", namespace: "weimeng/smartthings-haiku", author: "weimeng") {
-        capability "Fan Speed"
         capability "Switch"
+        capability "Switch Level"
 
         command "off"
         command "on"
-        command "setFanSpeed"
+        command "setLevel"
 	}
 
 
@@ -26,31 +26,32 @@ metadata {
 	}
 
 	tiles(scale :2) {
-        standardTile("fan", "device.fanSpeed") {
-            state "0", label: "Off", action: "on",  icon: "st.Lighting.light24"
-            state "1", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "2", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "3", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "4", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "5", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "6", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-            state "7", label: 'Speed ${currentValue}', action: "off", icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
+        standardTile("fan", "device.switch") {
+            state "on", icon: "st.Lighting.light24"
+            state "off", icon: "st.Lighting.light24"
         }
 
-        multiAttributeTile(name: "fanPower", type: "generic", width: 2, height: 2) {
-            tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
-                attributeState "on", action: "off", label: 'On', icon:"st.Lighting.light24", backgroundColor: "#00a0dc"
-                attributeState "off", action: "on", label: 'Off', icon:"st.Lighting.light24", backgroundColor: "#ffffff"
-            }
+        standardTile("fanPowerOn", "device.switch", height: 2, width: 3, decoration: "flat") {
+            state "on", label: "On", action: "on", icon: "st.Lighting.light24"
+            state "off", label: "On", action: "on", icon: "st.Lighting.light24"
         }
 
-		controlTile("fanSpeed", "device.fanSpeed", "slider", height: 2, width: 6, range: "(1..7)") {
-            state "fanSpeed", action: "setFanSpeed"
+        standardTile("fanPowerOff", "device.switch", height: 2, width: 3, decoration: "flat") {
+            state "on", label: "Off", action: "off", icon: "st.samsung.da.RC_ic_power"
+            state "off", label: "Off", action: "off", icon: "st.samsung.da.RC_ic_power"
+        }
+
+		controlTile("fanSpeed", "device.level", "slider", height: 2, width: 6) {
+            state "level", action: "setLevel"
+        }
+
+        valueTile("lastKnownFanSpeed", "device.fanSpeed", height: 2, width: 6) {
+            state "val", label: 'Last known fan speed: ${currentValue}'
         }
 
         main("fan")
 
-        details(["fanPower", "fanSpeed"])
+        details(["fanPowerOn", "fanPowerOff", "fanSpeed", "lastKnownFanSpeed"])
 	}
 }
 
@@ -72,7 +73,9 @@ def on() {
     sendRequest("<" + deviceMac + ";FAN;PWR;ON>")
 }
 
-def setFanSpeed(speed) {
+def setLevel(level) {
+    sendEvent(name: "level", value: level)
+    Integer speed = convertPercentToSpeed(level)
     sendEvent(name: "fanSpeed", value: speed)
     sendRequest("<" + deviceMac + ";FAN;SPD;SET;" + speed + ">")
 }
@@ -80,7 +83,7 @@ def setFanSpeed(speed) {
 // helper methods
 
 def sendRequest(message) {
-    def hosthex = convertIPtoHex(deviceIP)
+    def hosthex = convertIPToHex(deviceIP)
     def porthex = convertPortToHex(31415)
     device.deviceNetworkId = "$hosthex:$porthex"
     def cmds = []
@@ -88,7 +91,19 @@ def sendRequest(message) {
     return hubAction
 }
 
-private String convertIPtoHex(ipAddress) {
+private Integer convertPercentToSpeed(percent) {
+    Integer speed = Math.ceil(percent / 100.0 * 7)
+
+    // Make sure fan speed isn't higher than 7
+    speed = (speed > 7) ? 7 : speed
+
+    // Don't let fan speed be set to 0
+    speed = (speed < 1) ? 1 : speed
+
+    return speed
+}
+
+private String convertIPToHex(ipAddress) {
     String hex = ipAddress.tokenize( '.' ).collect {  String.format( '%02X', it.toInteger() ) }.join()
     return hex
 }
