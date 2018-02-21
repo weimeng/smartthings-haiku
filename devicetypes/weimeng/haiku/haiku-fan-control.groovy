fc347eb5db5b213d4dc7855a77463957af8ee4c0
Wei-Meng Lee <weimeng@staticfusion.org>
2018-02-21 16:53:59 +0800

    Update parent device control tiles

--- devicetypes/weimeng/haiku/haiku-fan-control.groovy
+++ devicetypes/weimeng/haiku/haiku-fan-control.groovy
@@ -2,6 +2,20 @@ metadata {
     definition(name: "Haiku Fan", namespace: "weimeng/smartthings-haiku", author: "weimeng") {
         capability "Switch"
         capability "Switch Level"
+
+        attribute "fanSpeed", "number"
+        attribute "fanSwitch", "enum", ["on", "off"]
+
+        attribute "lightLevel", "number"
+        attribute "lightSwitch", "enum", ["on", "off"]
+
+        command "fanOn"
+        command "fanOff"
+        command "fanSpeed"
+
+        command "lightOn"
+        command "lightOff"
+        command "lightLevel"
     }
 
     tiles(scale: 2) {
@@ -10,7 +24,49 @@ metadata {
             state "off", icon: "st.Lighting.light24"
         }
 
+        multiAttributeTile(name: "vanity", type: "generic", width: 6, height: 4, canChangeIcon: true) {
+            tileAttribute("device.fanSwitch", key: "PRIMARY_CONTROL") {
+                attributeState "on", label: "", icon: "st.Lighting.light24", backgroundColor: "#00a0dc"
+                attributeState "off", label: "", icon: "st.Lighting.light24", backgroundColor: "#00a0dc"
+            }
+
+            tileAttribute ("device.lightSwitch", key: "SECONDARY_CONTROL") {
+               attributeState "on", label: "Controls are not aware of fan & light state", icon: "st.thermostat.thermostat-right"
+               attributeState "off", label: "Controls are not aware of fan & light state", icon: "st.thermostat.thermostat-right"
+            }
+        }
+
+        standardTile("fanPowerOn", "device.fanSwitch", height: 2, width: 2, decoration: "flat") {
+            state "on", label: "On", action: "fanOn", icon: "st.Lighting.light24", backgroundColor: "#00a0dc"
+            state "off", label: "On", action: "fanOn", icon: "st.Lighting.light24", backgroundColor: "#ffffff"
+        }
+
+        standardTile("fanPowerOff", "device.fanSwitch", height: 2, width: 2, decoration: "flat") {
+            state "on", label: "Off", action: "fanOff", icon: "st.samsung.da.RC_ic_power", backgroundColor: "#ffffff"
+            state "off", label: "Off", action: "fanOff", icon: "st.samsung.da.RC_ic_power", backgroundColor: "#00a0dc"
+        }
+
+		controlTile("fanSpeed", "device.fanSpeed", "slider", height: 2, width: 2, range: (1..7)) {
+            state "fanSpeed", action: "fanSpeed"
+        }
+
+        standardTile("lightPowerOn", "device.lightSwitch", height: 2, width: 2, decoration: "flat") {
+            state "on", label: "On", action: "lightOn", icon: "st.Lighting.light21", backgroundColor: "#00a0dc"
+            state "off", label: "On", action: "lightOn", icon: "st.Lighting.light21", backgroundColor: "#ffffff"
+        }
+
+        standardTile("lightPowerOff", "device.lightSwitch", height: 2, width: 2, decoration: "flat") {
+            state "on", label: "Off", action: "lightOff", icon: "st.samsung.da.RC_ic_power", backgroundColor: "#ffffff"
+            state "off", label: "Off", action: "lightOff", icon: "st.samsung.da.RC_ic_power", backgroundColor: "#00a0dc"
+        }
+
+        controlTile("lightLevel", "device.lightLevel", "slider", height: 2, width: 2, range: (1..16)) {
+            state "lightLevel", action: "lightLevel"
+        }
+
         main("haikuFan")
+
+        details(["vanity", "fanPowerOn", "fanSpeed", "fanPowerOff", "lightPowerOn", "lightLevel", "lightPowerOff"])
     }
 
     preferences {
@@ -20,15 +76,12 @@ metadata {
 }
 
 def installed() {
-    log.debug 'Installing...'
-    device.deviceNetworkId = getDeviceNetworkId()
     createChildDevices()
 }
 
 def updated() {
-    device.deviceNetworkId = getDeviceNetworkId()
+    // Create child devices if not yet created
     def childDevices = getChildDevices()
-
     if (childDevices.size() == 0) {
         createChildDevices()
     }
@@ -37,34 +90,45 @@ def updated() {
 // Fan
 
 def fanOn() {
+    log.debug "Turning fan on"
     sendCommand("<" + deviceMac + ";FAN;PWR;ON>")
+    sendEvent(name: "fanSwitch", value: "on")
 }
 
 def fanOff() {
+    log.debug "Turning fan off"
     sendCommand("<" + deviceMac + ";FAN;PWR;OFF>")
+    sendEvent(name: "fanSwitch", value: "off")
 }
 
-def fanSpeed(speed) {
-    log.debug "Trying to set fan speed to ${speed}"
+def fanSpeed(Integer speed) {
     sendCommand("<" + deviceMac + ";FAN;SPD;SET;" + speed + ">")
+    sendEvent(name: "fanSpeed", value: speed)
 }
 
 // Light
 
 def lightOn() {
+    log.debug "Turning light on"
     sendCommand("<" + deviceMac + ";LIGHT;PWR;ON>")
+    sendEvent(name: "lightSwitch", value: "on")
 }
 
 def lightOff() {
+    log.debug "Turning light off"
     sendCommand("<" + deviceMac + ";LIGHT;PWR;OFF>")
+    sendEvent(name: "lightSwitch", value: "off")
 }
 
-def lightLevel(level) {
-    log.debug "Trying to set light level to ${level}"
+def lightLevel(Integer level) {
     sendCommand("<" + deviceMac + ";LIGHT;LEVEL;SET;" + level + ">")
+    sendEvent(name: "lightLevel", value: level)
 }
 
-private sendCommand(message) {
+// Helper methods
+
+private sendCommand(String message) {
+    device.deviceNetworkId = getDeviceNetworkId()
     def hubAction = new physicalgraph.device.HubAction(message, physicalgraph.device.Protocol.LAN)
     sendHubCommand(hubAction)
 }
