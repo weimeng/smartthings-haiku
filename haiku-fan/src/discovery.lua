9fcb528f1f7d929785c2d6ae0383c8b09df791ff
Wei-Meng Lee <weimeng@staticfusion.org>
2022-10-09 15:18:06 +0800

    Initial commit for SmartThings Edge driver

---
+++ haiku-fan/src/discovery.lua
@@ -0,0 +1,75 @@
+local socket = require("socket")
+local log = require('log')
+local datastore = require("datastore")
+
+local discovery = {}
+
+function discovery.start(driver, opts, cons)
+  -- Get own address
+  local s = assert(socket.udp())
+  local host = s:getsockname()
+
+  -- Get broadcast address
+  local broadcast_address = host:match("%d+%.%d+%.%d+%.") .. "255"
+
+  -- Broadcast discovery message
+  log.info("Sending discovery message")
+  s:setoption("broadcast", true)
+  s:settimeout(5)
+  s:sendto("<ALL;DEVICE;ID;GET>", broadcast_address, 31415)
+
+  -- Receive and store raw responses. Response data is as follows:
+  -- (<LABEL>;DEVICE;ID;<MAC_ADDRESS>;FAN,LSERIES)
+  local devices = {}
+  repeat
+    local data, ip, port = s:receivefrom()
+    log.info("Received: " .. data)
+
+    if data then
+      devices[ip] = {
+        data = data,
+      }
+    end
+  until not data
+
+  s:close()
+
+  -- Parse data
+  for ip, values in next, devices do
+    local tmp_data = string.sub(values['data'], 2, -2) .. ';'
+
+    fields = {}
+    for field in tmp_data:gmatch("(.-);") do
+      table.insert(fields, field)
+    end
+
+    devices[ip]['label'] = fields[1]
+    devices[ip]['mac_address'] = fields[4]
+    devices[ip]['vendor_provided_label'] = fields[5]
+  end
+
+  ds = datastore.init()
+
+  -- Create devices
+  for ip, values in next, devices do
+    local metadata_fan = {
+      type = "LAN",
+      device_network_id = values['mac_address'] .. "-FAN",
+      label = values['label'],
+      profile = "haiku-fan",
+      vendor_provided_label = ip, -- Hack to store IP address during creation
+    }
+    driver:try_create_device(metadata_fan)
+
+    local metadata_light = {
+      type = "LAN",
+      device_network_id = values['mac_address'] .. "-LGT",
+      label = values['label'] .. " Light",
+      profile = "haiku-fan-light",
+      vendor_provided_label = ip, -- Hack to store IP address during creation
+    }
+    driver:try_create_device(metadata_light)
+  end
+end
+
+return discovery
---
+++ haiku-fan/src/init.lua
@@ -0,0 +1,28 @@
+local capabilities = require("st.capabilities")
+local Driver = require("st.driver")
+
+-- Local imports
+local discovery = require("discovery")
+local lifecycles = require("lifecycles")
+local commands = require("commands")
+
+-- Driver definition
+local driver = Driver("Haiku Fan", {
+  discovery = discovery.start,
+  lifecycle_handlers = lifecycles,
+  supported_capabilities = {
+    capabilities.switch,
+    capabilities.switchLevel
+  },
+  capability_handlers = {
+    [capabilities.switch.ID] = {
+      [capabilities.switch.commands.on.NAME] = commands.switch_on,
+      [capabilities.switch.commands.off.NAME] = commands.switch_off
+    },
+    [capabilities.switchLevel.ID] = {
+      [capabilities.switchLevel.commands.setLevel.NAME] = commands.set_level
+    },
+  },
+})
+
+driver:run()
