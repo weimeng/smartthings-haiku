92bfd22c3b3dd48d83ce1823e407bbaaabb50f99
Wei-Meng Lee <weimeng@staticfusion.org>
2022-10-10 00:08:04 +0800

    Refactor commands

--- haiku-fan/src/commands.lua
+++ haiku-fan/src/commands.lua
@@ -1,80 +1,56 @@
 local capabilities = require("st.capabilities")
 local socket = require("socket")
 
+-- Local imports
+local helpers = require("helpers")
+
 local command_handler = {}
 
-function command_handler.switch_on(_driver, device, command)
-  if command_handler.is_fan(device) then
-    command_handler.send_command("FAN;PWR;ON", device)
+function command_handler.switch_on(_driver, device, _command)
+  local message
+  if helpers.is_fan(device) then
+    message = "FAN;PWR;ON"
   else
-    command_handler.send_command("LIGHT;PWR;ON", device)
+    message = "LIGHT;PWR;ON"
   end
 
+  command_handler.send_command(message, device)
   device:emit_event(capabilities.switch.switch.on())
 end
 
-function command_handler.switch_off(_driver, device, command)
-  if command_handler.is_fan(device) then
-    command_handler.send_command("FAN;PWR;OFF", device)
+function command_handler.switch_off(_driver, device, _command)
+  local message
+  if helpers.is_fan(device) then
+    message = "FAN;PWR;OFF"
   else
-    command_handler.send_command("LIGHT;PWR;OFF", device)
+    message = "LIGHT;PWR;OFF"
   end
 
+  command_handler.send_command(message, device)
   device:emit_event(capabilities.switch.switch.off())
 end
 
 function command_handler.set_level(_driver, device, command)
-  if command_handler.is_fan(device) then
-    local speed = command_handler.convert_percent_to_fan_speed(command.args.level)
-    command_handler.send_command("FAN;SPD;SET;" .. speed, device)
+  local level = command.args.level
+
+  local message
+  if helpers.is_fan(device) then
+    message = "FAN;SPD;SET;" .. helpers.convert_percent_to_fan_speed(level)
   else
-    local level = command_handler.convert_percent_to_light_level(command.args.level)
-    command_handler.send_command("LIGHT;LEVEL;SET;" .. level, device)
+    message = "LIGHT;LEVEL;SET;" .. helpers.convert_percent_to_light_level(level)
   end
 
-  device:emit_event(capabilities.switchLevel.level(command.args.level))
-end
-
-function command_handler.is_fan(device)
-  local device_suffix = string.sub(device.device_network_id, 19, -1)
-
-  if device_suffix == 'FAN' then
-    return true
-  end
-
-  return false
+  command_handler.send_command(message, device)
+  device:emit_event(capabilities.switchLevel.level(level))
 end
 
 function command_handler.send_command(message, device)
-  local device_ip = device:get_field('ip_address')
-  local device_mac = device:get_field('mac_address')
+  local ip_address = device:get_field('ip_address')
+  local mac_address = device:get_field('mac_address')
 
   local udp = socket.udp()
-  udp:sendto("<" .. device_mac .. ";" .. message .. ">", device_ip, 31415)
-end
-
-function command_handler.convert_percent_to_fan_speed(percent)
-  local speed = math.ceil(percent / 100.0 * 7)
-
-  -- Make sure fan speed isn't higher than 7
-  if speed > 7 then return 7 end
-
-  -- Don't let fan speed be set to 0
-  if speed < 1 then return 1 end
-
-  return speed
-end
-
-function command_handler.convert_percent_to_light_level(percent)
-  local level = math.ceil(percent / 100.0 * 16)
-
-  -- Make sure light level isn't higher than 16
-  if level > 16 then return 16 end
-
-  -- Don't let light level be set to 0
-  if level < 1 then return 1 end
-
-  return level
+  udp:sendto("<" .. mac_address .. ";" .. message .. ">", ip_address, 31415)
+  udp:close()
 end
 
 return command_handler
--- haiku-fan/src/helpers.lua
+++ haiku-fan/src/helpers.lua
@@ -8,6 +8,30 @@ function helpers.convert_light_level_to_percent(level)
   return level / 16.0 * 100
 end
 
+function helpers.convert_percent_to_fan_speed(percent)
+  local speed = math.ceil(percent / 100.0 * 7)
+
+  -- Make sure fan speed isn't higher than 7
+  if speed > 7 then return 7 end
+
+  -- Don't let fan speed be set to 0
+  if speed < 1 then return 1 end
+
+  return speed
+end
+
+function helpers.convert_percent_to_light_level(percent)
+  local level = math.ceil(percent / 100.0 * 16)
+
+  -- Make sure light level isn't higher than 16
+  if level > 16 then return 16 end
+
+  -- Don't let light level be set to 0
+  if level < 1 then return 1 end
+
+  return level
+end
+
 function helpers.is_fan(device)
   local device_suffix = string.sub(device.device_network_id, 19, -1)
 
